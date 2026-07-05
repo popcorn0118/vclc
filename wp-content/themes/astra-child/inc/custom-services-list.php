8ec22ef4402ebbcb72fe8ec98a183003f6f351c7
popcorn0118 <popcorn0118@gmail.com>
2026-07-05 12:49:27 +0800

    新增地點、服務項目相關

---
+++ wp-content/themes/astra-child/inc/custom-services-list.php
@@ -0,0 +1,10 @@
+<?php
+
+/* =================================
+
+   服務項目列表
+
+  "<?php require get_theme_file_path( 'inc/custom-services-list.php' ); ?>"
+  "[astra_custom_layout id=4937]"
+
+ * ================================== */
